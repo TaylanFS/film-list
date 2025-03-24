5dd9880b578a23a916c80dd36f1b63724513f936
Taylan <ferreirataylan123@gmail.com>
2025-03-24 04:35:05 -0300

    finished project

--- src/components/FilmList/index.tsx
+++ src/components/FilmList/index.tsx
@@ -5,18 +5,18 @@ import './index.scss'
 import { useEffect, useState } from 'react'
 import FilmCard from '../FilmCard'
 import { FilmType } from '../../types/film'
-
-
+import ReactLoading from 'react-loading'
 
 export default function FilmList() {
     const [films, setFilms] = useState<FilmType[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
 
     useEffect(() => {
         getFilms()
     }, [])
 
-    const getFilms = () => {
-        axios({
+    const getFilms = async () => {
+        await axios({
             method: 'get',
             url: 'https://api.themoviedb.org/3/discover/movie',
             params: {
@@ -26,7 +26,17 @@ export default function FilmList() {
         }).then((response) => {
             setFilms(response.data.results)
             
-        })
+        });
+
+        setLoading(false)
+    }
+
+    if (loading) {
+        return (
+            <div className='loading'>
+                <ReactLoading type="spin" color="#6046ff" height={"5%"} width={"5%"} />
+            </div>
+        )
     }
 
     return (
